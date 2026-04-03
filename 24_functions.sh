ca3870671aa295e9007045878aa8da60d5968799
MohammadFahad1 <fahad4bangladesh@gmail.com>
2026-04-03 15:44:20 +0600

    Learned about function declaration and call

---
+++ 24_functions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function welcomeNote1 {
+	echo "Hello world"
+}
+
+welcomeNote() {
+	echo "---------------------------"
+	echo "Welcome"
+	echo "---------------------------"
+}
+
+# To call our function
+welcomeNote
+welcomeNote
+welcomeNote
+welcomeNote
